2f4bbb7827f3a788c10f71f4296cf9ed5f1ead40
Carlos Fonsêca <cc_bf@hotmail.com>
2019-04-03 17:30:44 -0300

    Criando o controller e realizando algumas modificações no arquivo de conexão

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/noderest", { useMongoClient: true });
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose
+  .connect("mongodb://localhost/noderest")
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch(err => {
+    console.log("Not Connected to Database ERROR! ", err);
+  });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
